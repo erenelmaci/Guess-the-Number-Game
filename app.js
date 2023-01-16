610eb4dc67628183bc47b26a3cbf55934d35e35b
Eren Elmacı <erenx1623@gmail.com>
2023-01-17 01:11:46 +0300

    updt

--- app.js
+++ app.js
@@ -14,59 +14,39 @@ checkButton.addEventListener("click", () => {
   kalanHak--;
   kalanHakBildir.innerText = kalanHak;
 
-  console.log(tahminInput.value);
-  console.log(rastgeleSayi);
-
   if (tahminInput.value == rastgeleSayi) {
     sonucBildir.innerText = `Tebrikler Bildiniz Sayı: ${rastgeleSayi}`;
     outputContainer.appendChild(sonucBildir);
     checkButton.disabled = true;
     tahminInput.disabled = true;
-  }
-
-
-  else if (kalanHak <= 0) kalanHak = 1;
-
-
+  } else if (kalanHak <= 0) kalanHak = 1;
   else if (tahminInput.value === "") {
     sonucBildir.innerText = "Lütfen Bir Sayı Giriniz";
     outputContainer.appendChild(sonucBildir);
     tahminInput.value = "";
-  }
-  
-  else if (tahminInput.value < rastgeleSayi) {
+  } else if (tahminInput.value < rastgeleSayi) {
     min = tahminInput.value;
     sonucBildir.innerText = `${min} - ${max} Arasında bir sayı tutunuz`;
     outputContainer.appendChild(sonucBildir);
     tahminInput.value = "";
-  }
-  
-  
-  else if (tahminInput.value > rastgeleSayi) {
+  } else if (tahminInput.value > rastgeleSayi) {
     max = tahminInput.value;
     sonucBildir.innerText = `${min} - ${max} Arasında bir sayı tutunuz`;
     outputContainer.appendChild(sonucBildir);
     tahminInput.value = "";
-  }
-  
-  else if (kalanHak === 0) {
+  } else if (kalanHak == 0) {
     sonucBildir.innerText = `Tahmin Hakkınız Bitmiştir Sayı: ${rastgeleSayi}`;
     outputContainer.appendChild(sonucBildir);
     tahminInput.value = "";
-  }
-
-  else if (tahminInput.value.length === 0) {
+  } else if (tahminInput.value.length === 0) {
     sonucBildir.innerText = "Lütfen Geçerli bir sayı giriniz";
     outputContainer.appendChild(sonucBildir);
-  } 
-  else if (tahminInput.value > 100) {
+  } else if (tahminInput.value > 100) {
     sonucBildir.innerText = "Lütfen 0 - 100 arasında bir sayı giriniz";
     outputContainer.appendChild(sonucBildir);
     tahminInput.value = "";
     return;
   }
-
-
 });
 
 tahminInput.addEventListener("keydown", (e) => {
